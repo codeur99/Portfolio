3a90cf09f2ad0f0f3636024ddeea45ab9791e197
Lebomo jessy <Lebomojessy63@gmail.com>
2024-02-22 18:16:50 +0100

    portfolio

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+      <link rel="stylesheet" href="./styles/mon-site-portfolio.css"/>
+
+</head>
+<body>
+   <nav class= menu-bloc>
+<p class="formation-certificat-zhc">
+    <br/>
+    Contact
+    <br/>
+    </p>
+    <p class="formation-certificat-hc2">
+    <br/>
+     Formation &amp; Certificat
+    <br/>
+    </p>
+    <p class="travaux-raliss-cU6">
+    <br/>
+    Travaux réalisés
+    <br/>
+    </p>
+    <p class="mes-comptences-KNW">
+    <br/>
+    Mes compétences
+    <br/>
+    </p>
+    <p class="mes-services-2Gv">
+    <br/>
+    Mes services
+    <br/>
+    </p>
+    <p class="a-propos-de-moi-XzN">
+    <br/>
+    A propos de moi
+    <br/>
+    </p>
+    </nav>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,32 @@
+<nav class= menu-bloc>
+<p class="formation-certificat-zhc">
+    <br/>
+    Contact
+    <br/>
+    </p>
+    <p class="formation-certificat-hc2">
+    <br/>
+     Formation &amp; Certificat
+    <br/>
+    </p>
+    <p class="travaux-raliss-cU6">
+    <br/>
+    Travaux réalisés
+    <br/>
+    </p>
+    <p class="mes-comptences-KNW">
+    <br/>
+    Mes compétences
+    <br/>
+    </p>
+    <p class="mes-services-2Gv">
+    <br/>
+    Mes services
+    <br/>
+    </p>
+    <p class="a-propos-de-moi-XzN">
+    <br/>
+    A propos de moi
+    <br/>
+    </p>
+    </nav>
