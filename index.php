d4d6c3cea76c28658c580b0b6b9e669a6a89e9c6
Lebomo jessy <Lebomojessy63@gmail.com>
2024-02-26 10:17:42 +0100

    portfolio

--- index.php
+++ index.php
@@ -13,11 +13,108 @@
 
 <body>
 
-    <header>
-        <?php include("logo.php");?>
-        <?php include("menu.php");?>
-    </header>
- 
+  <header>
+      <?php include("logo.php");?>
+      <?php include("menu.php");?>
+  </header>
+
+  <div class="nom-section">
+      <span>
+          A propos de moi
+          <br/>
+      </span>
+  </div>
+
+  <section class="premiere-section">  
+      <div class="bloc-photo-et-paragraphe">  
+          <img class="bloc-a-propos-image" src="Images\whatsapp-image-2024-02-12-1542-1.png"/>
+          <p class="bloc-a-propos-paragraphe">
+              Hello! je m’appelle Lebomo Jessy Christopher<br/> Je suis développeur d’application web et mobile, j’implemente la logique metier ou encore la logique business, qui pour moi traduit les objectifs réels de l’application.
+          </p>
+      </div>
+      <div class="fond-Bloc-presentation"></div>        
+  </section>
+
+  <div class="fond-Bloc-separation"></div>        
+
+  <section class="deuxieme-section">
+        <div class="nom-section">
+            <span>
+                Mes services
+                <br/>
+            </span>
+        </div>
+
+        <div class="services-groupes">    
+            <div class="dev-web">
+                <span class="service-nom">
+                    Développement de Sites web<br/>
+                </span> 
+                <img class="image" src="Images\image-dvelopement-site.png"/>         
+            </div>
+
+            <div class="dev-mobile">
+                <span class="service-nom">
+                    Développement d'application mobile<br/>
+                </span> 
+                <img class="image" src="Images/image-dev-apps.png"/>
+            </div>
+        </div>
+
+        <div class="services-groupes">    
+            <div class="Maquettage-web-et-mobile">
+                <span class="service-nom">
+                    Maquettage d'application<br/>
+                </span> 
+                <img class="image" src="https://user.oc-static.com/upload/2022/06/17/16554553276644_P1C2-2%20(1).png"/>
+            </div>
+
+            <div class="Redaction-de-cahier-de-charges">
+                <span class="service-nom">
+                    Rédaction de cahier des charges<br/>
+                </span> 
+                <img class="image" src="Images\image-cahier-de-charge.png"/>
+            </div>
+        </div>
+  </section>
+
+  <div class="fond-Bloc-separation"></div>        
+
+    <section class="troisieme-section">
+        <div class="nom-section">
+            <span>
+                Mes compétences
+                <br/>
+            </span>
+        </div>
+
+        <div class="group-1-competence"> 
+            <div>
+                <button class="bouton-competence">Maquettage d'appliocation</button>
+            </div>
+            <div>
+                <button class="bouton-competence">Réalisation de sites statique et dynamique</button>
+            </div>
+            <div>
+                <button class="bouton-competence">Concevoir et déployer une base de données</button>
+            </div>
+        </div>
+
+        <div class="group-2-competence">
+            <div>
+                <button class="bouton-competence">Développement mobile</button>
+            </div>
+            <div>
+                <button class="bouton-competence">Assurer l'intégration et le deployement continu d'une Application Web</button>
+            </div>
+            <div>
+                <button class="bouton-competence">Documenter le deployement d'une App Web et Mobile</button>
+            </div>
+        </div>
+  </section>
+  
+   <div class="fond-Bloc-separation"></div>        
+
 </body>
 
 
--- menu.php
+++ menu.php
@@ -1,14 +1,8 @@
 <nav class="menu-header">
     <div class="menu-groupe-1">
-        <a class="menu-bouton" href="#">Accueil</a>
-        <a class="menu-bouton" href="#">A propos de moi</a>
-        <a class="menu-bouton" href="#">Mes services</a>
-        <a class="menu-bouton" href="#">Mes compétences</a>
+        <a class="menu-bouton" href="#"><span class="color-texte-menu">Accueil</span></a>
+        <a class="menu-bouton" href="#"><span class="color-texte-menu">Mes compétences</span></a>
+        <a class="menu-bouton" href="#"><span class="color-texte-menu">Mes travaux réalisées</span></a>
+        <a class="menu-bouton" href="#"><span class="color-texte-menu">Contacts</span></a>
    </div> 
-   <div class="menu-groupe-2">
-        <a class="menu-bouton" href="#">Travaux réalisés</a>
-        <a class="menu-bouton" href="#">Formations & Certificats</a>
-        <a class="menu-bouton" href="#">Contacts</a>
-   </div>
-   
 </nav>
